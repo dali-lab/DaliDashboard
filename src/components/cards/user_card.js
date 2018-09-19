e27bfba19fd7d826a9c3e1023e95fdc53018b6fc
ijemmao <ijemmao>
2018-09-19 12:16:15 -0400

    fixed box shadow

--- src/components/cards/user_card.js
+++ src/components/cards/user_card.js
@@ -12,10 +12,11 @@ class UserCard extends Component {
           browserHistory.push(routeStr);
         }
       }>
-        <img className="rounded" src={this.props.member.photoUrl} alt="profile" />
+        <img className="rounded large" src={this.props.member.photoUrl} alt="profile" />
         <div>
-          <h4 id="title"> {this.props.member.fullName} </h4>
-          <p id="detail"> {this.props.member.skills.join(' - ')} </p>
+          <h4 className="member-name"> {this.props.member.fullName} </h4>
+          {/* <p id="detail"> {this.props.member.skills.join(' - ')} </p> */}
+          <p className="member-detail">Developer</p>
           <a className="more-information" href="#">view profile</a>
         </div>
       </div>
