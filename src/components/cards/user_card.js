fa39aadf6d244915522ab6cc5623ba8df2ebcc43
ijemmao <ijemmao>
2018-09-20 01:09:43 -0400

    fixed text alignment

--- src/components/cards/user_card.js
+++ src/components/cards/user_card.js
@@ -12,11 +12,13 @@ class UserCard extends Component {
           browserHistory.push(routeStr);
         }
       }>
+      <div>
         <img className="rounded large" src={this.props.member.photoUrl} alt="profile" />
-        <div>
           <h4 className="member-name"> {this.props.member.fullName} </h4>
           {/* <p id="detail"> {this.props.member.skills.join(' - ')} </p> */}
           <p className="member-detail">Developer</p>
+      </div>
+        <div>
           <a className="more-information" href="#">view profile</a>
         </div>
       </div>
